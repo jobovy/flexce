92808d9f93109e698f035359f9a0bb7efa8524dd
Jo Bovy <bovy@astro.utoronto.ca>
2020-11-23 11:46:12 -0500

    Some changes to the installation: copy the yields to the installation directory and make the fileio submodule work

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+import os, os.path
+from setuptools import setup
 
 NAME = 'flexCE'
 # do not use x.x.x-dev.  things complain.  instead use x.x.xdev
 VERSION = '1.0.1dev'
 RELEASE = 'dev' not in VERSION
+
+yld_files= []
+os.chdir('flexCE')
+for (dirpath, dirnames, filenames) in os.walk(os.path.join('data')):
+    yld_files+= [os.path.join(dirpath,file) for file in filenames]
+os.chdir('..')
 
 setup(name=NAME,
       version=VERSION,
@@ -12,4 +19,5 @@ setup(name=NAME,
       author_email='brett.h.andrews@gmail.com',
       url='https://github.com/bretthandrews/flexCE',
       license='MIT',
-      packages=['flexCE'])
+      packages=['flexCE','flexCE.fileio'],
+      package_data={'flexCE': yld_files})
